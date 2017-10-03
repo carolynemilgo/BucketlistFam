2ea1ee10f902a5308727b5c403a1ec5d2e948739
carolynemilgo <carolynemilgo@gmail.com>
2017-10-03 12:20:17 +0300

    updated animations

---
+++ app/src/main/java/com/example/carol/bucketlistfam/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.example.carol.bucketlistfam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.carol.bucketlistfam.utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class HomeActivity extends AppCompatActivity {
+    public static final String TAG="HomeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //to load to bottom navigation view for the hiome activity
+        setUpBottomNavigationView();
+    }
+
+    //customize bottom nav bar for use in all activities
+    private void setUpBottomNavigationView(){
+        Log.d(TAG,"setupBottomNavBarOnView");
+        BottomNavigationViewEx bottomNavigationViewEx=(BottomNavigationViewEx) findViewById(R.id.bottomNavigationView);
+        BottomNavigationViewHelper.setUpBottomNavigationView(bottomNavigationViewEx);//pass widget here which contains animations
+
+    }
+}
