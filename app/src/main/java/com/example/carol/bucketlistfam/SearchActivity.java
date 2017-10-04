a6480a4ed59b8a53ebe018ed0be45b7ed3697ed1
carolynemilgo <carolynemilgo@gmail.com>
2017-10-04 09:12:39 +0300

    Created layouts for user signup and logins

---
+++ app/src/main/java/com/example/carol/bucketlistfam/SearchActivity.java
@@ -0,0 +1,20 @@
+package com.example.carol.bucketlistfam;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by carol on 10/3/17.
+ */
+
+public class SearchActivity extends AppCompatActivity{
+    private static final String TAG="search activity";
+
+        @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_home);
+
+        }
+}
